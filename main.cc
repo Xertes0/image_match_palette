f90aa1f5c1691ba1ec2f4f47c40ba0483e6180d7
Piotr Stefański <55015543+Xertes0@users.noreply.github.com>
2022-12-23 22:21:34 +0100

    CLI + no hard coded palettes + paths + parallel
    Loads the palette and file paths from command line arguments.
    Pixels are calculated in parallel.
    Included sample gruvbox palette from morhetz/gruvbox-contrib on GitHub.

--- main.cc
+++ main.cc
@@ -1,82 +1,71 @@
-#include <boost/gil/io/write_view.hpp>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <execution>
+#include <filesystem>
+#include <fstream>
+#include <ios>
 #include <limits>
+#include <sstream>
+#include <string_view>
+#include <vector>
 
 #include <boost/gil.hpp>
-#include <boost/gil/extension/io/png/tags.hpp>
-#include <boost/gil/typedefs.hpp>
 #include <boost/gil/extension/io/png.hpp>
+#include <boost/gil/extension/io/png/tags.hpp>
+#include <boost/gil/io/write_view.hpp>
+#include <boost/gil/typedefs.hpp>
 
 namespace gil = boost::gil;
+using image_t = gil::rgb8_image_t;
+using pixel_t = gil::rgb8_pixel_t;
 
-struct color_wrapper
+// using c++17 so can't use std::filesystem::path
+std::vector<pixel_t>
+load_palette(char const* path)
 {
-    color_wrapper(std::uint32_t val)
-    {
-        pixel[0] = (val & 0xFF'00'00) >> 16;
-        pixel[1] = (val & 0x00'FF'00) >> 8;
-        pixel[2] = (val & 0x00'00'FF) >> 0;
-    };
+    std::ifstream palette_file{path, std::ios_base::in};
 
-    operator
-    gil::rgb8_pixel_t()
-    {
-        return pixel;
+    palette_file.seekg(0, std::ios_base::end);
+    auto line_count = palette_file.tellg();
+    palette_file.seekg(0, std::ios_base::beg);
+
+    std::vector<pixel_t> palette{};
+    palette.reserve(line_count);
+
+    while(!palette_file.fail()) {
+        std::uint32_t hex{};
+        palette_file >> std::hex >> hex;
+
+        pixel_t pixel{};
+        pixel[0] = (hex & 0xFF'00'00U) >> 16;
+        pixel[1] = (hex & 0x00'FF'00U) >> 8;
+        pixel[2] =  hex & 0x00'00'FFU;
+
+        palette.emplace_back(std::move(pixel));
     }
 
-    gil::rgb8_pixel_t pixel;
-};
-
-std::array<color_wrapper, 37> colors
-{
-    0x1d2021,
-    0x282828,
-    0x32302f,
-    0x3c3836,
-    0x504945,
-    0x665c54,
-    0x7c6f64,
-    0x928374,
-    0x928374,
-    0xf9f5d7,
-    0xfbf1c7,
-    0xf2e5bc,
-    0xebdbb2,
-    0xd5c4a1,
-    0xbdae93,
-    0xa89984,
-    0xfb4934,
-    0xb8bb26,
-    0xfabd2f,
-    0x83a598,
-    0xd3869b,
-    0x8ec07c,
-    0xfe8019,
-    0xcc241d,
-    0x98971a,
-    0xd79921,
-    0x458588,
-    0xb16286,
-    0x689d6a,
-    0xd65d0e,
-    0x9d0006,
-    0x79740e,
-    0xb57614,
-    0x076678,
-    0x8f3f71,
-    0x427b58,
-    0xaf3a03,
-};
+    return palette;
+}
 
 int
-main()
+main([[maybe_unused]] int argc, char** argv)
 {
+    std::fprintf(stderr, "Usage: ./main <palette_path> <input_path.png> <output_path.png>\n");
+    std::fprintf(stderr, "Palette format: rgb in hex in each line\n");
+    std::fprintf(stderr, "Hope you entered it right 'couse i sure ain't checking it\n");
+
+    auto palette = load_palette(argv[1]);
+    std::printf("Loaded %zu colors from palette\n", palette.size());
+
     gil::rgb8_image_t image{};
-    gil::read_image("in.png", image, gil::png_tag{});
+    gil::read_image(argv[2], image, gil::png_tag{});
+
     auto view = gil::view(image);
-    for(auto& pixel : view) {
-        gil::rgb8_pixel_t closest = colors[0];
+    std::for_each(std::execution::par_unseq, view.begin(), view.end(), [&palette](auto& pixel){
+        ::pixel_t closest = palette[0];
         std::int32_t min_offset = std::numeric_limits<std::int32_t>::max();
-        for(gil::rgb8_pixel_t color : colors) {
+        for(::pixel_t color : palette) {
             std::int32_t offset =
                 std::abs(static_cast<std::int32_t>(pixel[0]) - static_cast<std::int32_t>(color[0])) +
                 std::abs(static_cast<std::int32_t>(pixel[1]) - static_cast<std::int32_t>(color[1])) +
@@ -87,7 +76,9 @@ main()
             }
         }
         pixel = closest;
-    }
-    gil::write_view("out.png", view, gil::png_tag{});
+    });
+
+    gil::write_view(argv[3], view, gil::png_tag{});
+
     return 0;
 }
